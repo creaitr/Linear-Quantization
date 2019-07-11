d5fd8e9ee2123e65c7c9756bcbdf2f90e0752287
DESKTOP-C3GTVSJ\Jin-Bae Park <cometonf@naver.com>
2019-07-11 17:37:56 +0900

    rename ImageNet models

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,4 +1,4 @@
 from .ResNet18 import Model as ResNet18
 from .AlexNet import Model as AlexNet
-from .ResNet18-ImageNet import Model as ResNet18-ImageNet
-from .AlexNet-ImageNet import Model as AlexNet-ImageNet
+from .ResNet18_ImageNet import Model as ResNet18_ImageNet
+from .AlexNet_ImageNet import Model as AlexNet_ImageNet
