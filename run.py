7c3983945fa8eb3c3494d41470ac8e01faa3afc6
DESKTOP-C3GTVSJ\Jin-Bae Park <cometonf@naver.com>
2019-09-14 17:33:40 +0900

    edit Configs

--- run.py
+++ run.py
@@ -1,11 +1,19 @@
 import os
 import sys
+import json
 
 gpu = '0'
 
+if len(sys.argv) == 2:
+    gpu = sys.argv[1]
+  
+with open('./config{0}.json'.format(gpu[0])) as f:
+    best = json.load(f)
+
 cmd = 'python train.py config config{0}.json gpu={1}'.format(gpu[0], gpu)
 
-cmd = cmd + ' logdir=ResNet18_{0}'
+cmd = cmd + ' logdir={}'.format(best['logdir']) + '_{0}'
 
 for i in range(2):
     r = os.system(cmd.format(i))
+
