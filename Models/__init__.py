8fc257957aaa9975b8801538b2d78e4d453ab234
DESKTOP-C3GTVSJ\Jin-Bae Park <cometonf@naver.com>
2019-10-01 21:51:39 +0900

    add MnasNet

--- Models/__init__.py
+++ Models/__init__.py
@@ -7,3 +7,6 @@ from .ResNet34 import Model as ResNet34
 from .ResNet34_ImageNet import Model as ResNet34_ImageNet
 from .ResNet50 import Model as ResNet50
 from .ResNet50_ImageNet import Model as ResNet50_ImageNet
+
+
+from .MnasNet import Model as MnasNet
